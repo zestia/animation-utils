ef9f57b72e3fbbd376153e611c3198649ce7f1ac
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-09-30 22:47:03 +0100

    use modules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 2.0.0
+
+- Use modules
+
 ## 1.0.0
 
 - Initial release
--- index.js
+++ index.js
@@ -1,61 +1,41 @@
-/* eslint-disable */
-
-(function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define('@zestia/animation-utils', factory);
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory();
-  } else {
-    global.animationUtils = factory();
-  }
-})(this, function () {
-  'use strict';
-
-  function waitForTransition(element, propertyName) {
-    return new Promise((resolve) => {
-      function handler(event) {
-        if (element !== event.target) {
-          return;
-        }
-
-        if (propertyName && propertyName !== event.propertyName) {
-          return;
-        }
-
-        element.removeEventListener('transitionend', handler);
-        resolve();
+export function waitForTransition(element, propertyName) {
+  return new Promise((resolve) => {
+    function handler(event) {
+      if (element !== event.target) {
+        return;
       }
 
-      element.addEventListener('transitionend', handler);
-    });
-  }
-
-  function waitForAnimation(element, animationName) {
-    return new Promise((resolve) => {
-      function handler(event) {
-        if (element !== event.target) {
-          return;
-        }
-
-        if (animationName && animationName !== event.animationName) {
-          return;
-        }
-
-        element.removeEventListener('animationend', handler);
-        resolve();
+      if (propertyName && propertyName !== event.propertyName) {
+        return;
       }
 
-      element.addEventListener('animationend', handler);
-    });
-  }
+      element.removeEventListener('transitionend', handler);
+      resolve();
+    }
 
-  function waitForFrame() {
-    return new Promise(requestAnimationFrame);
-  }
+    element.addEventListener('transitionend', handler);
+  });
+}
 
-  return {
-    waitForAnimation: waitForAnimation,
-    waitForTransition: waitForTransition,
-    waitForFrame: waitForFrame
-  };
-});
+export function waitForAnimation(element, animationName) {
+  return new Promise((resolve) => {
+    function handler(event) {
+      if (element !== event.target) {
+        return;
+      }
+
+      if (animationName && animationName !== event.animationName) {
+        return;
+      }
+
+      element.removeEventListener('animationend', handler);
+      resolve();
+    }
+
+    element.addEventListener('animationend', handler);
+  });
+}
+
+export function waitForFrame() {
+  return new Promise(window.requestAnimationFrame);
+}
