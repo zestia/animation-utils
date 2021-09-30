39a0c5cac695e2e70bd6e83075411ee5aaca6706
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-09-30 21:02:25 +0100

    add todo

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const test = require('ava');
-const {
-  waitForFrame,
-  waitForAnimation,
-  waitForTransition
-} = require('./index');
+// const {
+//   waitForFrame,
+//   waitForAnimation,
+//   waitForTransition
+// } = require('./index');
+
+test('waitForAnimation', (t) => {
+  t.is(true, true);
+});
