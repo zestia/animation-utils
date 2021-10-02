c03fe615aeeeb24afaa35830068bc7d5a623e8c9
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-10-03 00:17:41 +0100

    headless test

--- testem.js
+++ testem.js
@@ -2,5 +2,10 @@ module.exports = {
   framework: 'qunit',
   test_page: 'test/index.html',
   launch_in_dev: ['Chrome'],
-  launch_in_ci: ['Chrome']
+  launch_in_ci: ['Chrome'],
+  browser_args: {
+    Chrome: {
+      ci: ['--headless', '--remote-debugging-port=9222']
+    }
+  }
 };
